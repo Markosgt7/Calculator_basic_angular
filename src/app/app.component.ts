f9adccafbd8d60feaf74d83cab855545bd46317b
Markosgt7 <markos.guate7@gmail.com>
2023-08-02 15:16:39 -0600

    moulo de resultado configurado y funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'MI PRIMER CALCULADORA';
-  resultado: number =0;
+  resultadoPadre: number;
 
-  procesaResultado(resultado: number){
-    this.resultado= resultado;
+  procesaResultado(resultado: number) {
+    this.resultadoPadre = resultado;
   }
- 
 }
--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -3,15 +3,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
-  styleUrls: ['./formulario.component.css']
+  styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent {
-  numero1: number = 0;
-  numero2: number = 0;
+  numero1: number;
+  numero2: number;
   @Output() resultadoSuma = new EventEmitter<number>();
-  sumar():void {
-    let resultado = this.numero1 + this.numero2
-    this.resultadoSuma.emit(resultado)
+  sumar(): void {
+    let resultado = this.numero1 + this.numero2;
+    this.resultadoSuma.emit(resultado);
   }
-
 }
